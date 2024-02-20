b9ba444e9d655b01b6616a78a464ef17c4da869f
Rushikesh Patil <prushikeh@gmail.com>
2024-02-20 21:35:09 +0530

    Added assignment part 2

---
+++ Assignment 4/dynamic.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int *ptr = new int[size];
+    cout << "Enter " << size << " integers:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> ptr[i];
+    }
+
+    cout << "You entered: ";
+    for (int i = 0; i < size; ++i) {
+        cout << ptr[i] << " ";
+    }
+    cout <<endl;
+
+    delete ptr;
+    for (int i = 0; i < size; ++i) {
+        cout << ptr[i] << " ";
+    }
+
+    return 0;
+}
