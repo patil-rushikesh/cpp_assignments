36b9b8fe9bd82013793d553957c6571041314f21
Rushikesh Patil <prushikeh@gmail.com>
2024-02-27 14:39:57 +0530

    Modified 7th assignment

--- Assignment 7/7bii.cpp
+++ Assignment 7/7bii.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+static int x = 123456000;
 class bankAccount
 {
     string name;
@@ -10,7 +10,8 @@ class bankAccount
 
 public:
     bankAccount();
-    bankAccount(string a, int b, string c, float d);
+    bankAccount(string a, int b, string c, float d);\
+    void display();
     void deposit(float amt);
     void withdraw(float amt);
     friend class bankManager;
@@ -21,6 +22,7 @@ class bankManager
 public:
     void display(bankAccount z);
     void deleteAccount(bankAccount &z);
+    int ac_no(bankAccount z);
 };
 
 bankAccount::bankAccount()
@@ -34,11 +36,14 @@ bankAccount::bankAccount()
 bankAccount::bankAccount(string a, int b, string c, float d)
 {
     name = a;
-    acc_no = b;
+    acc_no = x+b;
     type = c;
     bal = d;
 }
-
+int bankManager::ac_no(bankAccount z)
+{
+    return z.acc_no;
+}
 void bankManager::display(bankAccount z)
 {
     cout << "Name of Customer : " << z.name << endl
@@ -46,7 +51,13 @@ void bankManager::display(bankAccount z)
          << "Type of Account : " << z.type << endl
          << "Balance : " << z.bal << " Rs." << endl;
 }
-
+void bankAccount::display()
+{
+    cout << "Name of Customer : " << name << endl
+         << "Account Number : " << acc_no << endl
+         << "Type of Account : " << type << endl
+         << "Balance : " << bal << " Rs." << endl;
+}
 void bankManager::deleteAccount(bankAccount &z)
 {
     z.name = "NULL";
@@ -84,131 +95,141 @@ int main()
     int i = 0;
     int choice, pos;
     string a;
+    int m;
     int b;
     string c;
     float d;
-    while(1){
-    cout << "Login as a customer (0) or Manager (1) else (2) to exit ?" << endl;
-    cout << "Enter Choice : ";
-    cin >> pos;
-    switch (pos)
+    while (1)
     {
-    case 0:
-        eloop = true; // Set eloop to true to enter the loop
-        while (eloop) // Continue looping as long as eloop is true
+        cout << "Login as a customer (0) or Manager (1) else (2) to exit ?" << endl;
+        cout << "Enter Choice : ";
+        cin >> pos;
+        switch (pos)
         {
-            cout << "1. Deposit money\n2. Withdraw money\n3. Exit\n";
-            cout << "Enter Choice  :";
-            cin >> choice;
-
-            switch (choice)
+        case 0:
+            eloop = true; // Set eloop to true to enter the loop
+            while (eloop) // Continue looping as long as eloop is true
             {
-            case 1:
-                cout << "Enter customer account number to deposit money: ";
-                cin >> b;
-                if (b >= 0 && b < i)
-                {
-                    cout << "Enter amount to deposit: ";
-                    cin >> d;
-                    customer[b].deposit(d);
-                }
-                else
-                {
-                    cout << "Invalid Account Number." << endl;
-                }
-                break;
+                cout << "1. Deposit money\n2. Withdraw money\n3. Exit\n";
+                cout << "Enter Choice  :";
+                cin >> choice;
 
-            case 2:
-                cout << "Enter customer Account number to withdraw money: ";
-                cin >> b;
-                if (b >= 0 && b < i)
+                switch (choice)
                 {
-                    cout << "Enter amount to withdraw: ";
-                    cin >> d;
-                    customer[b].withdraw(d);
-                }
-                else
-                {
-                    cout << "Invalid Account Number." << endl;
-                }
-                break;
+                case 1:
+                    cout << "Enter customer account number to deposit money: ";
+                    cin >> m;
+                    b = m%123456;
+                    if (b >= 0 && b < i)
+                    {
+                        cout<<"Your Account Details : ";
+                        customer[b].display();
+                        cout << "Enter amount to deposit: ";
+                        cin >> d;
+                        customer[b].deposit(d);
+                    }
+                    else
+                    {
+                        cout << "Invalid Account Number." << endl;
+                    }
+                    break;
 
-            case 3:
-                cout << "Exiting as customer." << endl;
-                eloop = false; // Set eloop to false to exit the loop
-                break;
+                case 2:
+                    cout << "Enter customer Account number to withdraw money: ";
+                    cin >> m;
+                    b = m%123456;
+                    if (b >= 0 && b < i)
+                    {
+                        cout<<"Your Account Details : ";
+                        customer[b].display();
+                        cout << "Enter amount to withdraw: ";
+                        cin >> d;
+                        customer[b].withdraw(d);
+                    }
+                    else
+                    {
+                        cout << "Invalid Account Number." << endl;
+                    }
+                    break;
 
-            default:
-                cout << "Invalid choice. Please try again." << endl;
-                break;
+                case 3:
+                    cout << "Exiting as customer." << endl;
+                    eloop = false; // Set eloop to false to exit the loop
+                    break;
+
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+                }
             }
-        }
-        break;
+            break;
 
-    case 1:
-        eloop = true; // Set eloop to true to enter the loop
-        while (eloop) // Continue looping as long as eloop is true
-        {
-            cout << "1. Register a customer\n2. Display Data of Customer\n3. Delete Data\n4. Exit\n";
-            cout << "Enter Choice : ";
-            cin >> choice;
-
-            switch (choice)
+        case 1:
+            eloop = true; // Set eloop to true to enter the loop
+            while (eloop) // Continue looping as long as eloop is true
             {
-            case 1:
-                cout << "Enter Name : ";
-                cin >> a;
-                cout << "Enter type of account : ";
-                cin >> c;
-                cout << "Enter Initial Deposit : ";
-                cin >> d;
-                customer[i] = bankAccount(a, i, c, d);
-                cout << "Account number assigned to customer is : " << i << endl;
-                i++;
-                break;
+                cout << "1. Register a customer\n2. Display Data of Customer\n3. Delete Data\n4. Exit\n";
+                cout << "Enter Choice : ";
+                cin >> choice;
 
-            case 2:
-                cout << "Enter customer account number to display: ";
-                cin >> b;
-                if (b >= 0 && b < i)
+                switch (choice)
                 {
-                    x.display(customer[b]);
-                }
-                else
-                {
-                    cout << "Invalid Account Number." << endl;
-                }
-                break;
-            case 3:
-                cout << "Enter customer account number to delete: ";
-                cin >> b;
-                if (b >= 0 && b < i)
-                {
-                    x.deleteAccount(customer[b]);
-                }
-                else
-                {
-                    cout << "Invalid Account Number." << endl;
-                }
-                break;
-            case 4:
-                cout << "Exiting as customer." << endl;
-                eloop = false; // Set eloop to false to exit the loop
-                break;
+                case 1:
+                    cout << "Enter Name : ";
+                    cin >> a;
+                    cout << "Enter type of account : ";
+                    cin >> c;
+                    cout << "Enter Initial Deposit : ";
+                    cin >> d;
+                    customer[i] = bankAccount(a, i, c, d);
+                    cout << "Account number assigned to customer is : " << x.ac_no(customer[i]) << endl;
+                    i++;
+                    break;
 
-            default:
-                cout << "Invalid choice. Please try again." << endl;
-                break;
+                case 2:
+                    cout << "Enter customer account number to display: ";
+                    cin >> m;
+                    b = m%123456;
+                    if (b >= 0 && b < i)
+                    {
+                        x.display(customer[b]);
+                    }
+                    else
+                    {
+                        cout << "Invalid Account Number." << endl;
+                    }
+                    break;
+                case 3:
+                    cout << "Enter customer account number to delete: ";
+                    cin >> m;
+                    b = m%123456;
+                    if (b >= 0 && b < i)
+                    {
+                        x.deleteAccount(customer[b]);
+                    }
+                    else
+                    {
+                        cout << "Invalid Account Number." << endl;
+                    }
+                    break;
+                case 4:
+                    cout << "Exiting as customer." << endl;
+                    eloop = false; // Set eloop to false to exit the loop
+                    break;
+
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+                }
             }
+            break;
+        case 2:
+            return 0;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+            break;
         }
-        break;
-    case 2:
-        return 0;
-        break;
-    default:
-        cout << "Invalid choice. Please try again." << endl;
-        break;
-    }
     }
     return 0;
 }
